b40441c1c42dbf660bd1b27ad29e4372209df15e
AshirAzeem-Developer <ashir.dzynr@gmail.com>
2023-07-27 08:35:47 +0500

    Version1

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './Components/Navbar';
+import Card from './Components/Card';
+import Form from './Components/Form';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <br />
+      <br />
+      <br />
+      <Card />
+      <br />
+      <br />
+      <br />
+
+      <Form />
+      <br />
+      <br />
+
+    </>
+  );
+}
+export default App;
